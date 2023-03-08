c33585ac586c2af087d6381f043a93796697f2e8
FlorenciaPCR <florencia.colodro.rodriguez@gmail.com>
2023-03-08 20:06:54 -0300

    Estilos

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import style from '../style.module.css'
 
 function Card({ name , band}) {
   return (
-    <div>Hola {name} tu banda favorita es {band}</div>
+    <div className={style.card} >
+      Hola {name}!
+      <p>Sabemos que tu banda favorita es:</p>
+      <span className={style.span}>{band}</span>
+    </div>
   )
 }
 export default Card
